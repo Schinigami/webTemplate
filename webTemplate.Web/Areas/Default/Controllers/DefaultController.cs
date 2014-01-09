c522503e018537af00c9076f2cba35c97011cee7
chernikov <chernikov@gmail.com>
2014-01-09 17:02:48 +0200

    Init commit

---
+++ webTemplate.Web/Areas/Default/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webTemplate.Web.Controllers;
+
+namespace webTemplate.Web.Areas.Default.Controllers
+{
+    public abstract class DefaultController : BaseController
+    {
+
+    }
+}
