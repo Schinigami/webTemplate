0d1f5cb396ffd8f47be9b83409a759a37767fc23
chernikov <chernikov@gmail.com>
2014-01-09 17:22:38 +0200

    Initial Commit

---
+++ webTemplate.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="webTemplate.Web.MvcApplication" Language="C#" %>
---
+++ webTemplate.Web/Models/Mappers/MapperCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using webTemplate.Model;
+using webTemplate.Web.Models.ViewModels;
+using webTemplate.Web.Models.ViewModels.User;
+
+namespace webTemplate.Web.Models.Mappers
+{
+    public static class MapperCollection
+    {
+        public static class LoginUserMapper
+        {
+            public static void Init()
+            {
+                Mapper.CreateMap<User, LoginView>();
+                Mapper.CreateMap<LoginView, User>();
+            }
+        }
+
+        public static class UserMapper
+        {
+            public static void Init()
+            {
+                Mapper.CreateMap<User, RegisterUserView>();
+                Mapper.CreateMap<RegisterUserView, User>();
+
+                Mapper.CreateMap<User, UserView>();
+                Mapper.CreateMap<UserView, User>();
+            }
+        }
+    }
+}
